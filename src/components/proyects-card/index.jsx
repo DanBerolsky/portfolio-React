f43acf2fd2933a346ed71fde28ea6a6a05a1f5fe
Your Name <tekopc>
2022-10-26 02:43:26 -0300

    .0

--- src/components/proyects-card/index.jsx
+++ src/components/proyects-card/index.jsx
@@ -4,23 +4,14 @@ import './index.css'
 const Proyects = () => {
     return (
         <div className="proyects-card card">
-            <h5>Proyects</h5>
+            <h5>My proyects</h5>
             <div className='proyects'>
                 <div className='proyects__item'>
-                    1
+                    <h6 className='item__proyect-name'> Portfolio modelo</h6>
+                    <ion-icon name="git-branch-sharp"></ion-icon>
+                    <ion-icon name="star-outline"></ion-icon>
                 </div>
-                <div className='proyects__item'>
-                    1
-                </div>
-                <div className='proyects__item'>
-                    1
-                </div>
-                <div className='proyects__item'>
-                    1
-                </div>
-                <div className='proyects__item'>
-                    1
-                </div>
+                
             </div>
             
         </div>
