aa72e8b39277f830ca3b8fbe850d1388d0c690fa
Your Name <tekopc>
2022-11-07 23:45:29 -0300

    t2

--- src/components/info-card/index.jsx
+++ src/components/info-card/index.jsx
@@ -6,48 +6,48 @@ const Info = () => {
         <div className="info-card card">
             <div className='info-card__item'>
                 <ion-icon name="location-sharp"></ion-icon>
-                <div>
+                <div className='info-card__item-container'>
                     <div>Based in :</div>
                     <div>Argentina</div>
                 </div>
             </div>
             <div className='info-card__item'>
                 <ion-icon name="logo-github"></ion-icon>
-                <div>
+                <div className='info-card__item-container'>
                     <div>Github :</div>
                     <div>
                         <a className='contact-link' href="https://github.com/DanBerolsky"></a>
                         <ion-icon className='link-icon' name="link-outline"></ion-icon>
-                        DanBerolsky
+                        <div>DanBerolsky</div>
                     </div>
                 </div>
             </div>
             <div className='info-card__item'>
                 <ion-icon name="logo-linkedin"></ion-icon>
-                <div>
+                <div className='info-card__item-container'>
                     <div>LinkedIn :</div>
                     <div>
                         <a className='contact-link' href="https://www.linkedin.com/in/dan-berolsky/"></a>
                         <ion-icon name="link-outline" className='link-icon'></ion-icon>
-                        Dan Berolsky
+                        <div>Dan Berolsky</div>
                     </div>
                 </div>
             </div>
             <div className='info-card__item'>
                 <ion-icon name="code-sharp"></ion-icon>
-                <div>
+                <div className='info-card__item-container'>
                     <div>Dev :</div>
                     <div> Dan Berolsky</div>
                 </div>
             </div>
             <div className='info-card__item'>
                 <ion-icon name="mail-sharp"></ion-icon>
-                <div>
+                <div className='info-card__item-container'>
                     <div>Email :</div>
                     <div>
                         <a className='contact-link' href="https://mail.google.com/mail/?view=cm&fs=1&to=berolskydan@gmail.com"></a>
                         <ion-icon className='link-icon' name="link-outline"></ion-icon>
-                        berolskydan@gmail.com
+                        <div>berolskydan@gmail.com</div>
                     </div>
                 </div>
             </div>
